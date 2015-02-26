5e27df432aa3af6332a1cffd07dacd0af812d7e3
João S. O. Bueno <gwidion@gmail.com>
2015-02-25 23:52:29 -0300

    wsgi example

---
+++ exemplo_wsgi.py
@@ -0,0 +1,9 @@
+from wsgiref import simple_server
+
+def app(env, resp):
+    resp("200 OK", [("Content-type", "text/html")])
+    return ['<h1>Alô mundo!</h1>']
+
+
+server=simple_server.make_server("", 8000, app)
+server.serve_forever()
